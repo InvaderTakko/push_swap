0bc1ced1b6e1617b29bd69fe11aaa6f9013bfbde
Stefan Ruff <ruff1stefan@gmail.com>
2024-04-09 17:04:18 +0200

    a

--- checker.h
+++ checker.h
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 14:51:50 by sruff             #+#    #+#             */
-/*   Updated: 2024/04/05 17:28:34 by sruff            ###   ########.fr       */
+/*   Updated: 2024/04/09 12:44:07 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 # define CHECKER_H
 
 // # include "./libft/libft.h"
-// # include "./libft/get_next_line.h"
-// # include "./libft/ft_printf.h"
+# include "./libft/get_next_line.h"
+# include "./libft/ft_printf_bonus.h"
 # include "push_swap.h"
-# include "get_next_line.h"
+// # include "get_next_line.h"
+
+int	reading_loop(t_ps_node **a, t_ps_node **b, char **split_arg, int argc);
 
 #endif
